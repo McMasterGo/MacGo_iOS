764064403a754691523308f259c23431e70d7fba
David Elsonbaty <davidels@me.com>
2014-11-16 16:56:08 -0500

    Initial Commit

---
+++ MacGo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MacGo
+//
+//  Created by David Elsonbaty on 11/16/14.
+//  Copyright (c) 2014 David Elsonbaty. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MacGo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MacGo
+//
+//  Created by David Elsonbaty on 11/16/14.
+//  Copyright (c) 2014 David Elsonbaty. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
