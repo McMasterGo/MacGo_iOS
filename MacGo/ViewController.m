21096e3430b9509aa904ae6dfaf8b67912287b12
David Elsonbaty <dave@Davids-MacBook-Pro.local>
2015-01-16 08:40:04 -0500

    - UI Changes

--- MacGo/ViewController.m
+++ MacGo/ViewController.m
@@ -17,6 +17,8 @@
 @property (weak, nonatomic) IBOutlet UIView *containerView;
 @property (weak, nonatomic) IBOutlet UILabel *titleLabel;
 @property (weak, nonatomic) IBOutlet UIImageView *qrCodeImageView;
+@property (weak, nonatomic) IBOutlet UIButton *refreshButton;
+@property (weak, nonatomic) IBOutlet UIButton *accountHistoryButton;
 
 @end
 
@@ -25,10 +27,13 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-//    [PFUser logOut];
-    self.containerView.layer.borderWidth = 1;
-    self.containerView.layer.borderColor = [[UIColor darkGrayColor] CGColor];
-    
+//   [PFUser logOut];
+    self.containerView.layer.borderWidth = 2;
+    self.containerView.layer.borderColor = [[UIColor whiteColor] CGColor];
+    self.refreshButton.layer.borderWidth = 2;
+    self.refreshButton.layer.borderColor = [[UIColor whiteColor] CGColor];
+    self.accountHistoryButton.titleLabel.textAlignment = NSTextAlignmentCenter;
+    self.view.backgroundColor = [UIColor colorWithRed:0.05 green:0.1 blue:0.11 alpha:1];
 }
 
 - (void)viewDidAppear:(BOOL)animated{
@@ -50,10 +55,21 @@
     [logInViewController.logInView.signUpButton removeFromSuperview];
     [logInViewController.logInView.dismissButton removeFromSuperview];
     [logInViewController.logInView.passwordForgottenButton removeFromSuperview];
+    [logInViewController.logInView.logInButton removeFromSuperview];
+    
+    logInViewController.logInView.backgroundColor = [UIColor blackColor];
+    logInViewController.logInView.usernameField.backgroundColor = [UIColor clearColor];
+    logInViewController.logInView.passwordField.backgroundColor = [UIColor clearColor];
+    logInViewController.logInView.usernameField.textAlignment = NSTextAlignmentCenter;
+    logInViewController.logInView.passwordField.textAlignment = NSTextAlignmentCenter;
+    logInViewController.logInView.usernameField.textColor = [UIColor whiteColor];
+    logInViewController.logInView.passwordField.textColor = [UIColor whiteColor];
     
     UILabel *logo = [[UILabel alloc] initWithFrame:logInViewController.logInView.logo.frame];
     logo.text = @"MacGo";
     logo.font = [self.titleLabel.font fontWithSize:45];
+    logo.textAlignment = NSTextAlignmentCenter;
+    logo.textColor = [UIColor colorWithRed:0.82 green:0.76 blue:0.49 alpha:1];
     logInViewController.logInView.logo = logo;
     [self presentViewController:logInViewController animated:YES completion:nil];
     
@@ -66,6 +82,12 @@
 
 #pragma mark - QRCode Helpers
 - (IBAction)makePurchaseButtonClicked:(id)sender {
+    
+    if (![sender isKindOfClass:[UIButton class]]) {
+        return;
+    }
+    UIButton *button = (UIButton*)sender;
+    button.userInteractionEnabled = NO;
     
     PFObject *token = [PFObject objectWithClassName:@"Tokens"];
     token[@"user"] = [PFUser currentUser];
@@ -82,19 +104,22 @@
                 
                 dispatch_async(dispatch_get_main_queue(), ^{
 
+                    self.containerView.backgroundColor = [UIColor whiteColor];
                     self.qrCodeImageView.image = qrcode;
                     self.qrCodeImageView.hidden = NO;
                     self.makePurchaseButton.hidden = YES;
-                    self.containerView.layer.borderColor = [[UIColor greenColor] CGColor];
+                    self.containerView.layer.borderColor = [[UIColor colorWithRed:0.45 green:0.75 blue:0.49 alpha:1] CGColor];
                     
                 });
                 
                 dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(kSecondsTimers * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
 
+                    self.containerView.backgroundColor = [UIColor clearColor];
                     self.qrCodeImageView.image = nil;
                     self.qrCodeImageView.hidden = YES;
                     self.makePurchaseButton.hidden = NO;
-                    self.containerView.layer.borderColor = [[UIColor darkGrayColor] CGColor];
+                    self.containerView.layer.borderColor = [[UIColor whiteColor] CGColor];
+                    button.userInteractionEnabled = YES;
                     
                 });
                 
